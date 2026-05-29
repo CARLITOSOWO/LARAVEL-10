cb3c2535b1a1e477353fe2f2ae7944f58ebc4004
Carlos <trabajoscharlie@gmail.com>
2026-05-29 00:24:49 -0600

    COMO ENVIAR EMAILS CON LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CursoController; 
+use App\Http\Controllers\CursoController;
+use Illuminate\Support\Facades\Mail;//*
+use App\Mail\ContactanosMailable;//*
 
 Route::get('/', HomeController::class)->name('home');//OBTENCION DE RUTA POR MEDIO DEL CONTROLADOR 
 
@@ -12,8 +14,9 @@ Route::resource('cursos', CursoController::class);
 Route::view('nosotros', 'nosotros')->name('nosotros');//ESTA FORMA DE DEFINIR LAS VISTAS SOLO LA VAMOS A USAR CUANDO QUEREMOS MOSTRAR CONTENIDO ESTATICO
 
 
-
-
+Route::get('contactanos', function () {Mail::to('guerramuertec@gmail.com')->send(new ContactanosMailable);
+return "MENSAJE ENVIADO";
+})->name('contactanos');
 
 
 
